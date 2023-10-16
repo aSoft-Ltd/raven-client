b2305dc2e76719881d9f1ad46b2aa6f8d3609f34
andylamax <andylamax@programmer.net>
2023-10-16 14:55:28 +0300

    Could test registraion on the client side

--- flix/receiver/src/commonMain/kotlin/raven/FlixMailBoxOptions.kt
+++ flix/receiver/src/commonMain/kotlin/raven/FlixMailBoxOptions.kt
@@ -5,7 +5,6 @@ import kotlinx.coroutines.channels.Channel
 
 class FlixMailBoxOptions(
     val url: String,
-    val channel: Channel<String>,
     val scope: CoroutineScope
 ) {
     val endpoint by lazy { FlixMailEndpoint(url) }

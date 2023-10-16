10b76bde71260b56e134ea10c857afaa77ee3979
andylamax <andylamax@programmer.net>
2023-10-16 12:33:13 +0300

    Have a flix mailer that we can use for testing frontends now

--- flix/receiver/src/commonMain/kotlin/raven/FlixMailbox.kt
+++ flix/receiver/src/commonMain/kotlin/raven/FlixMailbox.kt
@@ -1,29 +1,37 @@
 package raven
 
 import koncurrent.Later
-import koncurrent.LaterPromise
 import koncurrent.TODOLater
+import koncurrent.later
+import kotlin.time.Duration.Companion.seconds
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.mapNotNull
 import kotlinx.coroutines.launch
 
 class FlixMailbox(private val options: FlixMailBoxOptions) : MailBox {
 
-    val channel = options.channel
+    val messages = MutableStateFlow<String?>(null)
 
     init {
         val source = EventSource(options.endpoint.mailbox())
         source.on("message") {
             options.scope.launch {
-                anticipate?.resolveWith(it.data)
-                channel.send(it.data)
+                messages.emit(it.data)
+                delay(1.seconds)
+                messages.emit(null)
             }
         }
     }
 
-    private var anticipate: LaterPromise<String>? = null
     fun anticipate(): Later<String> {
-        val mail = LaterPromise<String>()
-        anticipate = mail
-        return mail
+        val current = messages.value
+        return options.scope.later { messages.filterNotNull().filter { it != current }.first() }
     }
 
     override fun save(message: EmailMessage): Later<EmailMessage> = TODOLater()

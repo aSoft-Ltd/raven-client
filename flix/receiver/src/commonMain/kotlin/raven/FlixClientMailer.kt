eb8addcfbdfbc62d720cf9ec2616fa57b3fecc69
andylamax <andylamax@programmer.net>
2023-12-24 12:48:14 +0300

    Massive update welcoming K2

--- flix/receiver/src/commonMain/kotlin/raven/FlixClientMailer.kt
+++ flix/receiver/src/commonMain/kotlin/raven/FlixClientMailer.kt
@@ -4,6 +4,11 @@ import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import kollections.List
 import koncurrent.Later
+import koncurrent.later.then
+import koncurrent.later.andThen
+import koncurrent.later.andZip
+import koncurrent.later.zip
+import koncurrent.later.catch
 import koncurrent.later
 
 class FlixClientMailer(private val options: FlixClientMailerOptions) : Mailer {
--- flix/receiver/src/commonMain/kotlin/raven/FlixMailbox.kt
+++ flix/receiver/src/commonMain/kotlin/raven/FlixMailbox.kt
@@ -1,6 +1,11 @@
 package raven
 
 import koncurrent.Later
+import koncurrent.later.then
+import koncurrent.later.andThen
+import koncurrent.later.andZip
+import koncurrent.later.zip
+import koncurrent.later.catch
 import koncurrent.TODOLater
 import koncurrent.later
 import kotlin.time.Duration.Companion.seconds
